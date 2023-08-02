2fb2cd4d6dd7566d2d10534eebc747095ca6e998
Jerrell-Abrahams <jerrellabrahams50@gmail.com>
2023-08-02 13:53:58 +0200

    Interacted with Mp3 Json for related videos

--- backend.js
+++ backend.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
+const ytdl = require('ytdl-core');
+const fs = require("fs")
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.render("index")
+
+
+
+  
+
+app.get('/', async (req, res) => {
+
+  const songInfo = await ytdl.getInfo("https://music.youtube.com/watch?v=WMGVnpaBE-I&feature=share")
+
+  console.log(songInfo.related_videos[0].author.name)
+
+  // console.log(songInfo.videoDetails.title)
+  // console.log(songInfo.videoDetails.embed.iframeUrl)
+  res.render("index", {songDetails: songInfo})
 })
 
 
