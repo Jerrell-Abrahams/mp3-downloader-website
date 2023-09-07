11daef53a2b7910204c1f0c9c81b8602e9167716
Jerrell Abrahams <jerrellabrahams50@gmail.com>
2023-09-07 17:24:22 +0200

    Fixed the api error

--- backend.js
+++ backend.js
@@ -1,106 +1,91 @@
-const express = require('express')
-const app = express()
-const fs = require("fs")
-const ytdl = require('ytdl-core');
-var search = require('youtube-search');
-const bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+var search = require("youtube-search");
+const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 var opts = {
-  maxResults: 3,
-  key: "AIzaSyC_5kOz87VLstAVRTnTWCUynGd0_rY1GaA"
+  maxResults: 5,
+  key: "AIzaSyC_5kOz87VLstAVRTnTWCUynGd0_rY1GaA",
 };
-
 
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-
-
-
-app.get('/', async (req, res) => {
-  res.render("index_without_ejs", {link: ""})
-})
+app.get("/", async (req, res) => {
+  res.render("index_without_ejs", { link: "" });
+});
 
 app.get("/about_project", (req, res) => {
-
   fs.readFile("views/count.txt", "utf-8", (err, data) => {
     if (err) {
-      console.log(err)
+      console.log(err);
+    } else {
+      res.render("about_project", { count: data });
     }
-    else {
-      res.render("about_project", {count: data})
-    }
-  })
-    
-  
-})
+  });
+});
 
 app.post("/", async (req, res) => {
-  
-  const videoId = req.body.search
-  console.log(videoId)
+  const videoId = req.body.search;
+  console.log(videoId);
 
-  search(videoId, opts, async function(err, results) {
-    if(err) return console.log(err);
-  
-    console.dir(results);
-    var videoLink = results[0].link
-    const searched_videos = results
+  search(videoId, opts, async function (err, results) {
+    if (err) return console.log(err);
 
-    const songInfo = await ytdl.getInfo(videoLink)
+    console.log(results);
+    let videoLink = results[0].link;
+    const searched_videos = results;
 
+    const songInfo = await ytdl.getBasicInfo(videoLink);
 
-    res.render("index", {songDetails: songInfo, videos: searched_videos})
+    res.render("index", { songDetails: songInfo, videos: searched_videos });
   });
+});
 
-})
+app.post("/convert_mp3", async (req, res) => {
+  const videoId = req.body.videoID;
+  console.log(videoId);
 
-app.post('/convert_mp3', async (req, res) => {
-
-  const videoId = req.body.videoID
-  console.log(videoId)
-
-
-  const fetchAPI = await fetch("https://youtube-mp36.p.rapidapi.com/dl?id=" + videoId, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-key": '36410dff50msh6dc7a3cd892d33fp1e4e2ajsnb25fd6888dd0',
-      "x-rapidapi-host": 'youtube-mp36.p.rapidapi.com'
+  const fetchAPI = await fetch(
+    "https://youtube-mp36.p.rapidapi.com/dl?id=" + videoId,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "36410dff50msh6dc7a3cd892d33fp1e4e2ajsnb25fd6888dd0",
+        "x-rapidapi-host": "youtube-mp36.p.rapidapi.com",
+      },
     }
-  })
+  );
 
-  const fetchResponse = await fetchAPI.json()
+  const fetchResponse = await fetchAPI.json();
 
   if (fetchResponse.status === "ok") {
-    console.log(fetchResponse)
+    console.log(fetchResponse);
     fs.readFile("views/count.txt", "utf-8", (err, data) => {
       if (err) {
-        console.log(err)
-      }
-      else {
-        let retrievedNumber = parseInt(data)
-        retrievedNumber++ 
-        fs.writeFile("views/count.txt",
-        String(retrievedNumber), (err) => {
+        console.log(err);
+      } else {
+        let retrievedNumber = parseInt(data);
+        retrievedNumber++;
+        fs.writeFile("views/count.txt", String(retrievedNumber), (err) => {
           if (err) {
-            console.log(err)
+            console.log(err);
           }
-        })
+        });
       }
-    })
-    return res.redirect(fetchResponse.link)
+    });
+    return res.redirect(fetchResponse.link);
+  } else {
+    return res.redirect("/");
   }
-  else {
-    return res.redirect("/")
-  }
-})
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
